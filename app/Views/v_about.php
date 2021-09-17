382f6d468f5f0e31ff815a29f6342b658d47a1cc
RevitaSetianingsih27 <revitasetianingsih27@gmail.com>
2021-09-16 19:21:49 -0700

    first commit

---
+++ app/Views/v_about.php
@@ -0,0 +1,7 @@
+<div class="container mt-3">
+    <div class="text-center">
+        <img src="/assets/images/linux.png" alt="logo" width="180" class="img-thumbnail rounded-circle">
+        <h1 class="display-4"><strong>Praktikum Web Lanjut Dengan Studi Kasus Blog App</h1>
+        <p class="lead">Programming | CodeIgniter 4 | Php</p>
+    </div>
+</div>
