a5e3105056c416b0cfa10e0fdf5729f0673702a8
RevitaSetianingsih27 <revitasetianingsih27@gmail.com>
2021-09-23 13:09:08 -0700

    Templating and Update Posts

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -32,16 +32,17 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', function(){
-	$data = [
-		'title' => "Blog - Home"
-	];
-	echo view('layout/header', $data);
-	echo view('layout/header');
-	echo view('layout/navbar');
-	echo view('v_home');
-	echo view('layout/footer');
-});
+//$routes->get('/', function(){
+//	$data = [
+//		'title' => "Blog - Home"
+//	];
+//	echo view('layout/header', $data);
+//	echo view('layout/header');
+//	echo view('layout/navbar');
+//	echo view('v_home');
+//	echo view('layout/footer');
+//});
+$routes->get('/admin', 'Templating::index');
 $routes->get('/posts', 'PostController::index');
 
 $routes->get('/about', function(){
--- app/Views/v_posts.php
+++ app/Views/v_posts.php
@@ -2,4 +2,99 @@
     <div class="text-center">
         <h1>All Posts</h1>
     </div>
+
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <form action="" method="get">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Cari Postingan" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                    <button class="btn btn-outline-secondary" type="button">Find</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <div class="row mt-4">
+        <div class="col-md-4 mb-3">
+            <div class="card" >
+                <img class="card-img-top" src="https://source.unsplash.com/1600x900/?nature" alt="Card image">
+                    <div class="card-body">
+                        <a href="" class="text-decoration-none text-dark">
+                            <h5 class="card-title">Nature</h5>
+                        </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-3">
+            <div class="card" >
+                <img class="card-img-top" src="https://source.unsplash.com/1600x900/?technology" alt="Card image">
+                    <div class="card-body">
+                        <a href="" class="text-decoration-none text-dark">
+                            <h5 class="card-title">Technology</h5>
+                        </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-3">
+            <div class="card" >
+                <img class="card-img-top" src="https://source.unsplash.com/1600x900/?cat" alt="Card image">
+                    <div class="card-body">
+                        <a href="" class="text-decoration-none text-dark">
+                            <h5 class="card-title">Cat</h5>
+                        </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-3">
+            <div class="card" >
+                <img class="card-img-top" src="https://source.unsplash.com/1600x900/?food" alt="Card image">
+                    <div class="card-body">
+                        <a href="" class="text-decoration-none text-dark">
+                            <h5 class="card-title">Food</h5>
+                        </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-3">
+            <div class="card" >
+                <img class="card-img-top" src="https://source.unsplash.com/1600x900/?programming" alt="Card image">
+                    <div class="card-body">
+                        <a href="" class="text-decoration-none text-dark">
+                            <h5 class="card-title">Programming</h5>
+                        </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-3">
+            <div class="card" >
+                <img class="card-img-top" src="https://source.unsplash.com/1600x900/?lifestyle" alt="Card image">
+                    <div class="card-body">
+                        <a href="" class="text-decoration-none text-dark">
+                            <h5 class="card-title">Lifestyle</h5>
+                        </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-3">
+            <div class="card" >
+                <img class="card-img-top" src="https://source.unsplash.com/1600x900/?sport" alt="Card image">
+                    <div class="card-body">
+                        <a href="" class="text-decoration-none text-dark">
+                            <h5 class="card-title">Sport</h5>
+                        </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-3">
+            <div class="card" >
+                <img class="card-img-top" src="https://source.unsplash.com/1600x900/?water" alt="Card image">
+                    <div class="card-body">
+                        <a href="" class="text-decoration-none text-dark">
+                            <h5 class="card-title">Water</h5>
+                        </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    
 </div>
